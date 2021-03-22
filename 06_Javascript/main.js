601d46116e9e56cac09918d7736392d66bb290f8
havietco <job@haviet.co>
2021-03-22 23:26:42 +0700

    Cop bai

--- 06_Javascript/main.js
+++ 06_Javascript/main.js
@@ -1,1 +1,44 @@
-alert("Hello cả nhà");
+<SCRIPT LANGUAGE="JavaScript">
+    // Lav Mir
+<!-- Begin
+var themethod= prompt("Please enter celsius to fahrenheit(c to f) or fahrenheit to celsius(f to c)", "Not case sensitive")
+var thenewmethod= themethod.toLowerCase()
+function f2c(fahrenheit1)
+{
+var answer ;
+celsius1= 5/9 * (fahrenheit1 - 32)  
+return ;
+}
+
+function c2f(celsius2)
+{
+var answer ;
+var fahrenheit2 ; 
+fahrenheit2= 9/5 * celsius2 + 32
+return fahrenheit2 ;
+}
+
+switch (thenewmethod)
+{
+case "f to c":
+var thefahrenheit= prompt("Please enter the degrees in fahrenheit", " ") ; 
+f2c(thefahrenheit);
+answer= celsius1 ;
+alert(thefahrenheit + "  is " + answer)
+break;
+case "c to f":
+var thecelsius= prompt("Please enter the degrees in celsius", " ")
+var result= c2f(thecelsius);
+alert(+ thecelsius + " is " + result) ; 
+break;
+default:
+document.write("Please reload the page as there was an error")
+break;
+}
+//  End -->
+
+</script>
+	<!--
+    This script downloaded from www.JavaScriptBank.com
+Come to view and download over 2000 + free javascript at www.JavaScriptBank.com
+-->
---
+++ 08_Cau_lenh_if_eles/main.js
@@ -0,0 +1,5 @@
+let a = 5;
+let b = 7:
+
+let thongBao = (a > b && a< b) ? 'Đúng ' : 'sai';
+document.write(thongBao);
